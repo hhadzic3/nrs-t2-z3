9d5e25b5718739508725a6c12378126d83b00519
hhadzic3 <hhadzic3@etf.unsa.ba>
2020-03-16 10:00:35 +0100

    3. zadatak (pocetak)

---
+++ src/ba/unsa/etf/rpr/PretragaContoller.java
@@ -0,0 +1,13 @@
+package ba.unsa.etf.rpr;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
+public class PretragaContoller {
+    public TextField fldTrazi;
+    public ListView lista;
+
+    public void trazi(ActionEvent actionEvent) {
+    }
+}
